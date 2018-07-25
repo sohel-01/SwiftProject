5332a6dbbfc22a67d8820183b5e5b64f57461820
MacStudent <macstudent@20X-iMacXX-M-406.local>
2018-07-25 13:59:40 -0400

    Project Initials

---
+++ EmployeePayrollSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EmployeePayrollSwift
+//
+//  Created by MacStudent on 2018-07-25.
+//  Copyright © 2018 mdsp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
