14a9eedadc9298fee4636992b30a16f61a3c5a47
MacStudent <macstudent@20X-iMacXX-M-419.local>
2018-07-27 14:14:23 -0400

    commisionBased and fixedBased

---
+++ EmployeePayrollSwift/Entities/CommisionBasedPartTime.swift
@@ -0,0 +1,24 @@
+//
+//  CommisionBasedPartTime.swift
+//  EmployeePayrollSwift
+//
+//  Created by MacStudent on 2018-07-27.
+//  Copyright © 2018 mdsp. All rights reserved.
+//
+
+import Foundation
+class CommisionBasedPartTime
+{
+    var commisionPercent: Double!
+    var earning: Double!
+    
+    init(){
+        
+    }
+    func enterEmpDetails(){
+        print("commisionPercent:")
+       self.commisionPercent = Double(readLine()!)
+        print("earning:")
+       self.earning = Double(readLine()!)
+}
+}
---
+++ EmployeePayrollSwift/Entities/FixedBasedPartTime.swift
@@ -0,0 +1,22 @@
+//
+//  FixedBasedPartTime.swift
+//  EmployeePayrollSwift
+//
+//  Created by MacStudent on 2018-07-27.
+//  Copyright © 2018 mdsp. All rights reserved.
+//
+
+import Foundation
+class FixedBasedPartTime{
+    var fixedAmount: Double!
+    var earning: Double!
+    init(){
+        
+    }
+    func enterEmpDetails(){
+        print("fixedAmount:")
+       self.fixedAmount = Double(readLine()!)
+        print("earning:")
+       self.earning = Double(readLine()!)
+}
+}
